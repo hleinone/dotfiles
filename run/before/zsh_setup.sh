0edd8c3e62ec9dc3bee48b1b926f22359620ac08
Hannu Leinonen <hleinone@gmail.com>
2021-04-16 17:04:14 +0300

    Don't fail if already cloned.

--- run/before/zsh_setup.sh
+++ run/before/zsh_setup.sh
@@ -3,4 +3,5 @@ set -o errexit -o nounset
 
 curl -fsSL https://starship.rs/install.sh | bash
 
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+if cd "${HOME}/.zgen"; then git pull; else git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"; fi
+
