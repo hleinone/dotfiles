020a7ebf251b68a1893bfb841a1b600556add494
Hannu Leinonen <hleinone@gmail.com>
2021-10-22 14:45:38 +0300

    Remove tracking parameters.

--- symlinks/finicky/.finicky.js
+++ symlinks/finicky/.finicky.js
@@ -20,5 +20,24 @@ module.exports = {
       }
     },
   ],
+  rewrite: [
+    {
+      match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
+      url({ url }) {
+        const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
+        const removeKeys = ["fblid", "gclid"]; // Remove all query parameters matching these keys
+
+        const search = url.search
+          .split("&")
+          .map((parameter) => parameter.split("="))
+          .filter(([key]) => !removeKeysStartingWith.some((startingWith) => key.startsWith(startingWith)))
+          .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
+
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join("=")).join("&"),
+        };
+      }
+    }
   ]
 }
