ccf636a1c268946d25ca675c216a138a8344e119
Hannu Leinonen <hleinone@gmail.com>
2021-11-22 08:44:19 +0200

    Add missing space.

--- run/before/init.sh
+++ run/before/init.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset
 
 # Set the timezone; see `sudo systemsetup -listtimezones` for other values
-sudo systemsetup -settimezone "Europe/Helsinki" >/dev/null
+sudo systemsetup -settimezone "Europe/Helsinki" > /dev/null
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
