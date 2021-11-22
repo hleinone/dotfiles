d982520b4fb4f6b646ecc4ce8298f90fb5d2cb5c
Hannu Leinonen <hleinone@gmail.com>
2021-11-22 08:44:30 +0200

    Echo to stderr.

--- run/after/z_macos_setup.sh
+++ run/after/z_macos_setup.sh
@@ -5,7 +5,7 @@ REPOS_DIR="$(mktemp -d -t macos_setup_repos)"
 
 trap 'rm -rf "$REPOS_DIR"' EXIT
 (
-    echo "Installing PAM Touch ID…"
+    >&2 echo "Installing PAM Touch ID…"
     cd "$REPOS_DIR"
     set -x
     git clone https://github.com/Reflejo/pam-touchID.git
@@ -14,7 +14,7 @@ trap 'rm -rf "$REPOS_DIR"' EXIT
     set +x
 
     cd -
-    echo "Installing PAM Watch ID…"
+    >&2 echo "Installing PAM Watch ID…"
     set -x
     git clone https://github.com/biscuitehh/pam-watchid.git
     cd pam-watchid
@@ -22,27 +22,27 @@ trap 'rm -rf "$REPOS_DIR"' EXIT
     set +x
 )
 
-echo "Installing latest Node globally using nodenv…"
+>&2 echo "Installing latest Node globally using nodenv…"
 LATEST_NODE="$(nodenv install -l | ggrep -E '^[0-9\.]+$' | tail -1)"
 nodenv install $LATEST_NODE
 nodenv global $LATEST_NODE
 
-echo "Installing latest Python globally using pyenv…"
+>&2 echo "Installing latest Python globally using pyenv…"
 LATEST_PYTHON="$(pyenv install -l | sed 's/^ *//g' | ggrep -E '^[0-9\.]+$' | tail -1)"
 pyenv install "$LATEST_PYTHON"
 pyenv global "$LATEST_PYTHON"
 
-echo "Installing latest Ruby globally using rbenv…"
+>&2 echo "Installing latest Ruby globally using rbenv…"
 LATEST_RUBY="$(rbenv install -L | ggrep -E '^[0-9\.]+$' | tail -1)"
 rbenv install "$LATEST_RUBY"
 rbenv global "$LATEST_RUBY"
 
-echo "Installing latest Go globally using goenv…"
+>&2 echo "Installing latest Go globally using goenv…"
 LATEST_GO="$(goenv install -l | sed 's/^ *//g' | ggrep -E '^[0-9\.]+$' | tail -1)"
 goenv install "$LATEST_GO"
 goenv global "$LATEST_GO"
 
-echo "Setting up latest Java globally using jenv…"
+>&2 echo "Setting up latest Java globally using jenv…"
 for java in /Library/Java/JavaVirtualMachines/*; do
   jenv add "$java/Contents/Home/"
 done
@@ -50,11 +50,11 @@ LATEST_JAVA="$(jenv versions --bare | sed 's/^ *//g' | ggrep -E '^[0-9\.]+$' | t
 jenv global "$LATEST_JAVA"
 
 # Install Rust
-echo "Installing latest Rust…"
+>&2 echo "Installing latest Rust…"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install ESP-IDF
-echo "Installing ESP-IDF…"
+>&2 echo "Installing ESP-IDF…"
 mkdir -p ~/esp
 git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
 ~/esp/esp-idf/install.sh
@@ -97,12 +97,12 @@ duti -s com.sublimetext.4 .svg all
 duti -s org.videolan.vlc .wav all
 duti -s org.videolan.vlc .3gp all
 
-echo ""
-echo "\033[0;32mTurbo Boost Switcher Pro from: http://tbswitcher.rugarciap.com/\033[0m"
-echo "\033[0;32mInstall Additional Tools for Xcode from: https://developer.apple.com/download/more/\033[0m"
-echo "\033[0;32mInstall Smart Scroll from: https://www.marcmoini.com/sx_en.html\033[0m"
-echo "\033[0;32mInstall Android Studio, AppCode etc. from Jetbrains Toolbox\033[0m"
-echo ""
-echo "\033[0;32mTurn on VSCode settings sync\033[0m"
-echo "\033[0;32mTurn on BetterTouchTool settings sync\033[0m"
-echo "\033[0;32mTurn on Android Studio & AppCode settings sync\033[0m"
+>&2 echo ""
+>&2 echo "\033[0;32mTurbo Boost Switcher Pro from: http://tbswitcher.rugarciap.com/\033[0m"
+>&2 echo "\033[0;32mInstall Additional Tools for Xcode from: https://developer.apple.com/download/more/\033[0m"
+>&2 echo "\033[0;32mInstall Smart Scroll from: https://www.marcmoini.com/sx_en.html\033[0m"
+>&2 echo "\033[0;32mInstall Android Studio, AppCode etc. from Jetbrains Toolbox\033[0m"
+>&2 echo ""
+>&2 echo "\033[0;32mTurn on VSCode settings sync\033[0m"
+>&2 echo "\033[0;32mTurn on BetterTouchTool settings sync\033[0m"
+>&2 echo "\033[0;32mTurn on Android Studio & AppCode settings sync\033[0m"
