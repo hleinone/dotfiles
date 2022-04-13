0067a71d621223472f92c7aee714c0b2be9d366d
Hannu Leinonen <hleinone@gmail.com>
2022-04-13 14:19:12 +0300

    Update Starship installation script.

--- run/before/zsh_setup.sh
+++ run/before/zsh_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -o errexit -o nounset
 
-curl -fsSL https://starship.rs/install.sh | bash
+curl -sS https://starship.rs/install.sh | sh
 
 if cd "${HOME}/.zgenom"; then git pull; else git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"; fi
