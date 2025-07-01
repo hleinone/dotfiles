ab673509772b95f5eab3c6e7fa33f24f96f1ccc4
Hannu Leinonen <hleinone@gmail.com>
2025-07-01 11:11:49 +0300

    Updated Finicky configuration
    Finicky 4 has a bit different configuration. Removed rewrite for now.

--- symlinks/finicky/.finicky.js
+++ symlinks/finicky/.finicky.js
@@ -1,7 +1,13 @@
-// Use https://finicky-kickstart.now.sh to generate basic configuration
-// Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
+// @ts-check
 
-module.exports = {
+/**
+ * @typedef {import('/Applications/Finicky.app/Contents/Resources/finicky.d.ts').FinickyConfig} FinickyConfig
+ */
+
+/**
+ * @type {FinickyConfig}
+ */
+export default {
   defaultBrowser: "Brave Browser",
 
   handlers: [
@@ -14,7 +20,7 @@ module.exports = {
     },
     {
       // Open links in work profile when the option key is pressed
-      match: () => finicky.getKeys().option,
+      match: () => finicky.getModifierKeys().option,
       browser: { 
         name: "Brave Browser",
         profile: "Profile 1",
@@ -27,25 +33,5 @@ module.exports = {
         profile: "Default",
       }
     },
-  ],
-  rewrite: [
-    {
-      match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
-      url({ url }) {
-        const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
-        const removeKeys = ["fblid", "gclid"]; // Remove all query parameters matching these keys
-
-        const search = url.search
-          .split("&")
-          .map((parameter) => parameter.split("="))
-          .filter(([key]) => !removeKeysStartingWith.some((startingWith) => key.startsWith(startingWith)))
-          .filter(([key]) => !removeKeys.some((removeKey) => key === removeKey));
-
-        return {
-          ...url,
-          search: search.map((parameter) => parameter.join("=")).join("&"),
-        };
-      }
-    }
   ]
 }
